c880936f78c0e0b285321fcc7bb6f670011fa973
Frank <frankadesantis@gmail.com>
2021-05-16 11:36:58 -0400

    created hello.py

---
+++ Hello.py
@@ -0,0 +1,8 @@
+print("Hello User")
+print("What is your name?")
+myName = input()
+print("Hello " + myName +"!")
+print("The length of your name is: " + str(len(myName)))
+print("What is your age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in one year.")
