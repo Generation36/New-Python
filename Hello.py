c69f97e3fa4b3d82151ea38db129d684d38128c9
Frank <frankadesantis@gmail.com>
2021-06-14 15:04:04 -0400

    Updated 6/14/2021

--- Hello.py
+++ Hello.py
@@ -6,3 +6,5 @@ print("The length of your name is: " + str(len(myName)))
 print("What is your age?")
 myAge = input()
 print("You will be " + str(int(myAge) + 1) + " in one year.")
+
+#Updated 6/14/2021
