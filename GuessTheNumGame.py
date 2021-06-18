780e697a2e09e4cf1d9a42c6b2e065a50c118090
Frank <frankadesantis@gmail.com>
2021-06-18 15:22:57 -0400

    Updated 3:22pm 6/18/2021

--- GuessTheNumGame.py
+++ GuessTheNumGame.py
@@ -5,26 +5,31 @@ import sys
 #Function declarations
 def startGameSequence():
     secretNum = random.randint(1,10)
-    print("I am thinking of a number between 1 and 10.")
+    print(secretNum)
+
+    #prompts player to take a guess
+    print("I am thinking of a number between 1 and 10.\nYou have 3 guesses.")
+    playersGuess = input()
+
 
 #Greeting Message 
 print("Hello! What is your name?")
-
 name = input()
 
 print("It is nice to meet you " + name + "!")
 print("Would you like to play a game? (Y/N)")
 init = input()
 
+#Conditional Flow Control
 while init:
-    if init == "Y":
+    if init == "Y" or "y":
         startGameSequence()
         break
 
-    elif init == "N":
+    elif init == "N" or "n":
         sys.exit()
 
-    elif init != "Y" or "N":
+    elif init != "Y" or "N" or "y" or "n":
             print("Invalid Entry. Please enter a Y or N.")
             init = input()
 
